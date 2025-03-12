6165f0895409c7d76807fc7c1ee037e96bdc195e
Wellington Silva <wellington.71319@gmail.com>
2025-03-12 17:00:28 -0300

    Update R sandbox

--- sandbox.R
+++ sandbox.R
@@ -9,7 +9,7 @@ bound_remainder <- function(k_current_term, k_previous_term) {
 }
 
 # Log normalizing constant of the COM-Poisson distribution
-log_Z_com_poisson <- function(log_mu, nu, M = 10000, leps = -8 * log(2)) {
+log_Z_com_poisson <- function(log_mu, nu, M = 10000, leps = -52 * log(2)) {
   if (nu <= 0 || is.infinite(nu)) stop("nu must be positive and finite")
   
   log_Z_terms <- numeric(M)
@@ -51,7 +51,7 @@ log_posterior <- function(y, intercept, shape) {
 }
 
 # Metropolis-Hastings MCMC
-mcmc <- function(y, n_iter = 5000) {
+mcmc <- function(y, n_iter = 1000) {
   intercept <- 0
   shape <- 1
   
@@ -74,7 +74,7 @@ mcmc <- function(y, n_iter = 5000) {
     
     chain[i, ] <- c(intercept, shape)
     
-    if (i %% 100 == 0) cat("Iteration", i, "\n")
+    if (i %% 50 == 0) cat("Iteration", i, "\n")
   }
   
   return(chain)
@@ -84,8 +84,8 @@ mcmc <- function(y, n_iter = 5000) {
 zinb <- read.csv("https://paul-buerkner.github.io/data/fish.csv")
 
 # Run MCMC
-set.seed(123)
-chain <- mcmc(zinb$count, n_iter = 2000)
+set.seed(666)
+chain <- mcmc(zinb$count, n_iter = 1000)
 
 # Inspect results
 plot(chain[, 1], type = "l", main = "Intercept")
